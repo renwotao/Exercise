fb6dd2194c77c28d284f49f02b6bdae1057e4378
renwotao <852443964@qq.com>
2016-08-09 23:40:05 +0800

    function: def & import

---
+++ python/function.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+print('abs(100) = %d' % abs(100))
+print('abs(-20) = %d' % abs(-20))
+print('max(1, 2) = %d' % max(1, 2))
+print('max(2, 3, 1, -5) = %d' % max(2, 3, 1, -5))
+print("int('123') = %d" % int('123'))
+print('int(12.34) = %d' % int(12.34))
+print("float('12.34') = %f" % float('12.34'))
+print('str(1.23) = %s' % str(1.23))
+print('bool(1) = %s' % bool(1))
+print("bool('') = %s" % bool(''))
+
+a = abs
+print(a(-1))
