209ad5b4f6655658103ad5fb5d2766210d2517fa
renwotao <852443964@qq.com>
2016-08-27 21:57:16 +0800

    correct error in orm.py & creat models for build tables of users,blogs,comments

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -68,7 +68,7 @@ class Field(object):
         self.default = default
 
     def __str__(self):
-        return '<s%, %s:%s>' % (self.__class__, __name__, self.column_type, self.name)
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 class StringField(Field):
 
@@ -108,8 +108,8 @@ class ModelMetaclass(type):
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                logging.info('  found mapping: %ss ==> %s' % (k, v))
-                mapings[k] = v
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
                 if v.primary_key:
                     # 找到主键
                     if primaryKey:
@@ -128,8 +128,8 @@ class ModelMetaclass(type):
         attrs['__field__'] = fields # 除主键外的属性名
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f: '`%s`=?' % (mapping.get(f).name or f), fields)), primaryKey)
-        attrs['__delete__'] = 'delete from `%s` set %s where `%s`=?' % (tableName, primaryKey)
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
