004c344656f59c01d9ce733e79dbf8ac4fe5755b
renwotao <852443964@qq.com>
2016-08-28 15:49:54 +0800

    config files & handlers & templates dir

---
+++ awesome-python3-webapp/www/config_default.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': '123456',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
