9ac35c53e28f3a3dac4f66788a018d9e744b2217
renwotao <852443964@qq.com>
2016-11-13 21:41:31 +0800

    tcp server framework

--- network/framework/skel.h
+++ network/framework/skel.h
@@ -4,11 +4,11 @@
 /* UNIX version */
 #define INIT()          program_name = \
                         trrchr( argv[ 0 ], '/' ) ) ? \
-                        rogram_name++ : \
+                        program_name++ : \
                         program_name = argv[ 0 ] )
-#define EXIT(s)         xit( s )
-#define CLOSE(s)        f (　close( s ) ) error( 1, errno, \
-                        close failed" )
+#define EXIT(s)         exit( s )
+#define CLOSE(s)        if (　close( s ) ) error( 1, errno, \
+                        "close failed" )
 #define set_errno(e)    errno = ( e )
 #define isvalidsock(s)  ( ( s ) >= 0 )
 
